0f37f23336ca9818a47ca2c33c2821b96cf80930
Mano Sriram <mano.sriram0@gmail.com>
2019-01-17 17:44:29 +0530

    modified

--- client/src/components/ContactList.js
+++ client/src/components/ContactList.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 
 class ContactList extends Component {
-    componentDidMount() {
-        console.log(this.props.data)
-    }
+
     render() {
         return(
             <h1>Hey there</h1>
